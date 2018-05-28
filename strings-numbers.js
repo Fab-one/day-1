b3c14df7f39127d18030f6540cb83e5583d31052
Fab-one <info@fab-one.nl>
2018-05-28 17:44:56 +0200

    variables.js

---
+++ strings-numbers.js
@@ -0,0 +1,2 @@
+var firstName = "Wouter", lastName = "de Vos", age = "25";
+document.write(`${firstName} ${lastName} is ${age} years old`)
---
+++ variables.js
@@ -0,0 +1,14 @@
+
+
+let languageName = "Javascript";
+console.log(languageName);
+
+const myName = 'fabian'
+
+console.log(myName);
+
+const age = 41
+const ageInMonths = age * 12;
+
+console.log(`My age: ${age}`)
+console.log(`My age in months: ${ageInMonths}`)
